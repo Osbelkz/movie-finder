a9d73690118c37f6307a1e4d25d1bb099a4ba456
Askar <osbelkz@gmail.com>
2020-10-01 14:45:42 +0600

    fix: add useComponentVisible

--- src/components/Header/Search/Search.tsx
+++ src/components/Header/Search/Search.tsx
@@ -1,53 +1,65 @@
-import React, {ChangeEvent, useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect} from 'react';
 import classes from "./Search.module.css";
 import {useDispatch, useSelector} from "react-redux";
-import {getSearchResults, setSearchWord, showResults, StateType} from "../../../redux/searchReducer";
-import {StoreType} from "../../../redux/store";
+import {SearchStateType} from "../../../redux/searchReducer";
+import {RootStateType} from "../../../redux/store";
 import SearchItem from "./SearchItem/SearchItem";
-import {setMovieId} from "../../../redux/reducer";
+import {getSearchResultsTC, setSearchWordAC} from '../../../redux/search-actions';
+import {setMovieIdAC} from "../../../redux/movies-actions";
+import useComponentVisible from '../../../hooks/useComponentVisible';
 
 
 const Search = () => {
 
-    const search = useSelector<StoreType, StateType>(state => state.search)
-    const movieIdData = useSelector<StoreType>(state => state.movie.movieId)
+    const search = useSelector<RootStateType, SearchStateType>(state => state.search)
+    const movieIdData = useSelector<RootStateType>(state => state.movie.movieId)
     const dispatch = useDispatch()
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        dispatch(setSearchWord(e.target.value))
+        dispatch(setSearchWordAC({searchWord: e.target.value}))
     }
 
-    const onChangeCurrentMovie = (movieId: number) => {
+    const onChangeCurrentMovieCard = (movieId: number) => {
         if (movieIdData !== movieId) {
-            dispatch(setMovieId(movieId))
-            dispatch(showResults(false))
+            dispatch(setMovieIdAC({movieId}))
+            setIsComponentVisible(false)
         }
     }
 
+    const onFocus = () => {
+        setIsComponentVisible(true)
+    }
+
     useEffect(() => {
-        dispatch(getSearchResults(search.searchWord))
+        if (search.searchWord.length >= 3) {
+            dispatch(getSearchResultsTC(search.searchWord))
+        }
     }, [search.searchWord])
 
+
+    const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(true);
+
     return (
-        <div className={classes.search}>
+        <div className={classes.search} ref={ref}>
             <input type="text"
                    className={classes.searchInput}
-                   onFocus={() => dispatch(showResults(true))}
+                   onFocus={onFocus}
                    value={search.searchWord}
                    onChange={onChangeHandler}
             />
-            {search.showResults
-                ? <div className={classes.dropResults} >
-                    {search.searchResults?.results.sort((a,b)=>b.popularity-a.popularity).slice(0, 8).map(movie =>
-                        <SearchItem
-                            changeCurrentMovie={onChangeCurrentMovie}
-                            original_title={movie.original_title}
-                            key={movie.id}
-                            movie_id={movie.id}
-                            release_date={movie.release_date}
-                            vote_average={movie.vote_average}/>)}
+            {isComponentVisible && <div className={classes.dropResults}>
+                    {search.searchResults?.results
+                        .sort((a, b) => b.popularity - a.popularity)
+                        .slice(0, 8)
+                        .map(movie =>
+                            <SearchItem
+                                changeCurrentMovie={onChangeCurrentMovieCard}
+                                original_title={movie.original_title}
+                                key={movie.id}
+                                movie_id={movie.id}
+                                release_date={movie.release_date}
+                                vote_average={movie.vote_average}/>)}
                 </div>
-                : null
             }
 
         </div>
